13990fc776368e2f0a38e6d2d0b607e6ac0b65a4
Kunal Shah <kunal.kshah@hotmail.com>
2021-06-02 12:56:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+//manipulation of styles using jQuery
+// $("h1").css("color","green");
+// $("h1").addClass("title");
+// $("h1").addClass("title margin");
+// $("h1").removeClass("margin");
+//to check wheather a class exists
+// $("h1").hasClass("title");//this returns either true or false
+// $("button").css("background-color","red");
+//manipulation of text using jQuery
+// $("h1").text("Kunal");
+// $("button").text("Don't click me");
+//alternative way
+// $("h1").html("<em>Kunal</em>");
+//manipulation of attributes using jQuery
+// $("a").attr("href","https://www.bing.com");
+// $("a").text("Bing");
+//Adding event listeners using jQuery
+// $("h1").click(function () {
+//   $("h1").css("color","purple");
+// });
+// $("button").click(function () {
+//   $("h1").css("color","purple");
+// });
+// $("input").keypress(function (event) {
+//   console.log(event.key);
+// });
+// $(document).keypress(function (event) {
+//   $("h1").text(event.key);
+// });
+// $("h1").on("mouseover",function () {
+//   $("h1").css("color","green");
+// });
+//Animations using jQuery
+// $("button").click(function () {
+//   $("h1").hide();
+// });
+// $("button").click(function () {
+//   $("h1").toggle();
+// });
+// $("button").click(function () {
+//   $("h1").fadeOut();
+// });
+// $("button").click(function () {
+//   $("h1").fadeIn();
+// });
+// $("button").click(function () {
+//   $("h1").fadeToggle();
+// });
+// $("button").click(function () {
+//   $("h1").slideUp();
+// });
+// $("button").click(function () {
+//   $("h1").slideDown();
+// });
+// $("button").click(function () {
+//   $("h1").slideToggle();
+// });
